cbee4c4b5011dd040bc28f43badbd155ba7fc2c6
Sidorenko Vladimir <yoyavova@gmail.com>
2009-09-07 12:11:42 +0300

    basic setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name="stdimage",
+           version="0.1",
+           description="Django standarized image field.",
+           author="Marc Garcia",
+           packages=find_packages(),
+           include_package_data=True,
+)
+
